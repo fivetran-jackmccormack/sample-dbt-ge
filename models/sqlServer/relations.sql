de3733ebe5e5c28bdd6b688855935a36358237dc
Jack McCormack <jack.mccormack@jack.mccormack-YX4Q7JTW24>
2022-11-03 17:01:56 +0000

    adding where

--- models/sqlServer/relations.sql
+++ models/sqlServer/relations.sql
@@ -4,5 +4,5 @@
     )
 }}
 {%- set event_relations = dbt_utils.get_relations_by_pattern('cli%', 'teachers%') -%}
-{{ dbt_utils.union_relations(relations = event_relations) }}
+{{ dbt_utils.union_relations(relations = event_relations, where='1=1') }}
 
